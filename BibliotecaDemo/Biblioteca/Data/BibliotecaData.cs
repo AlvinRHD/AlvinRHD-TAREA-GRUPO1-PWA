97f6fab3dae3f805158ab151c0aad46ada056d55
AlvinRHD <ezequielhernandes907@gmail.com>
2024-02-07 19:28:23 -0600

    Agg proyecto nuevamente

---
+++ BibliotecaDemo/Biblioteca/Data/BibliotecaData.cs
@@ -0,0 +1,45 @@
+﻿using Biblioteca.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Biblioteca.Data
+{
+    public class BibliotecaData
+    {
+        string connectionString = "Data Source=SAVI5\\SQLEXPRESS;Initial Catalog=DBLibrary;Integrated Security=True;Trust Server Certificate=True";
+
+        public IEnumerable<BibliotecaModel> GetAll()
+        {
+            List<BibliotecaModel> BibliotecaList = new List<BibliotecaModel>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "SELECT B.LibroId, B.Nombre AS LibroNombre, B.PublishDate, B.ISBN, B.Genero, " +
+                                          "A.AutorId, A.Nombre AS AutorNombre, A.Edad, A.FechaNacimiento, A.Nacionalidad, " +
+                                          "E.EditorialId, E.Nombre AS EditorialNombre, E.AÑO AS EditorialAño, E.TipoEditorial, E.Ubicacion " +
+                                          "FROM Books B " +
+                                          "INNER JOIN Autor A ON B.AutorId = A.AutorId " +
+                                          "INNER JOIN Editorial E ON B.EditorialId = E.EditorialId;";
+
+                    command.CommandType = CommandType.Text;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            BibliotecaModel bibliotecaModel = new BibliotecaModel();
+                            
+
+                            BibliotecaList.Add(bibliotecaModel);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BibliotecaDemo/Biblioteca/Models/BibliotecaModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Biblioteca.Models
+{
+    public class BibliotecaModel
+    {
+        public class Autor
+        {
+            public int AutorId { get; set; }
+            public string Nombre { get; set; }
+            public string Edad { get; set; }
+            public int FechaNacimiento { get; set; }
+            public string Nacionalidad { get; set; }
+        }
+
+        public class Editorial
+        {
+            public int EditorialId { get; set; }
+            public string Nombre { get; set; }
+            public int AÑO { get; set; }
+            public string TipoEditorial { get; set; }
+            public string Ubicacion { get; set; }
+        }
+
+        public class Books
+        {
+            public int LibroId { get; set; }
+            public string Nombre { get; set; }
+            public int PublishDate { get; set; }
+            public int ISBN { get; set; }
+            public string Genero { get; set; }
+        }
+    }
+}
